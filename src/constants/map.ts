6b655ed68d5c446087cbba122f25dfccba3e9b5e
malikkahfi12 <malikkahfi1996@gmail.com>
2026-05-31 21:23:48 +0700

    Vector Tiles

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -1,4 +1,4 @@
-export const MAP_STYLE = "https://tiles.openfreemap.org/styles/liberty";
+export const MAP_STYLE = `https://api.maptiler.com/maps/streets-v2/style.json?key=${process.env.EXPO_PUBLIC_MAPTILER_KEY}`;
 
 export const INITIAL_CENTER: [number, number] = [106.8272, -6.1754];
 
