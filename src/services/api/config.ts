9120855071d95ecdaa4d874c57850d11b0a50629
malikkahfi12 <malikkahfi1996@gmail.com>
2026-06-10 12:29:48 +0700

    settings expo

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,12 +1,4 @@
-const config = {
-  development: {
-    apiUrl: process.env.EXPO_PUBLIC_API_URL,
-    apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  },
-  production: {
-    apiUrl: process.env.EXPO_PUBLIC_PROD_API_URL,
-    apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  },
+export const apiConfig = {
+  apiUrl: process.env.EXPO_PUBLIC_API_URL,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
 } as const;
-
-export const apiConfig = __DEV__ ? config.development : config.production;
